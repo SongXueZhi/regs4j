253b5c67698ea0a0b3f7e648a5be640a393cfa97
Siang Hwee <siang_hwee@hotmail.com>
2022-01-22 04:49:40 +0800

    Fix TestManager to only capture java exceptions

--- src/main/java/core/test/TestManager.java
+++ src/main/java/core/test/TestManager.java
@@ -7,20 +7,27 @@ import org.dom4j.io.SAXReader;
 import java.io.File;
 import java.util.List;
 import java.util.ArrayList;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class TestManager {
-    final static String REPORT_PATH = "target" + File.separator + "surefire-reports" + File.separator + "junitreports";
+    final static String REPORT_PATH = "target" + File.separator + "surefire-reports";
+    final static String JUNIT_REPORT_PATH = REPORT_PATH + File.separator + "junitreports";
     final static SAXReader reader = new SAXReader();
 
     public List<String> getErrors(File codePath) {
-        File reportDirectory = new File(codePath, REPORT_PATH);
+        File reportDirectory = new File(codePath, JUNIT_REPORT_PATH);
+        if (!reportDirectory.exists())
+            reportDirectory = new File(codePath, REPORT_PATH);
         if (!reportDirectory.exists() || !reportDirectory.isDirectory())
-            return null;
+        	return null;
         List<String> errorMessages = new ArrayList<>();
-        for (String report : reportDirectory.list()) {
-            String errorMessage = getError(new File(reportDirectory, report));
-            if (errorMessage != null)
-                errorMessages.add(errorMessage);
+        for (String file : reportDirectory.list()) {
+        	if (file.matches("TEST.*\\.xml")) {
+        		String errorMessage = getError(new File(reportDirectory, file));
+                if (errorMessage != null)
+                    errorMessages.add(errorMessage);
+        	}
         }
         return errorMessages;
     }
@@ -31,11 +38,27 @@ public class TestManager {
             Element root = doc.getRootElement();
             Element testCase = root.element("testcase");
             Element error = testCase.element("error");
-            if (error != null)
-                return error.attributeValue("type");
-            Element failure = testCase.element("failure");
-            if (failure != null)
-                return failure.attributeValue("type");
+            if (error == null)
+            	error = testCase.element("failure");
+            
+            if (error != null) {
+	            String errorMsg = error.attributeValue("type");
+	            if (errorMsg.matches("java.*")) {
+	            	return errorMsg;
+	            }
+	            String errorData = error.getData().toString();
+	            Pattern p = Pattern.compile("Caused by: (.+)\\s");
+            	Matcher m = p.matcher(errorData);
+            	if (m.find()) {
+            		String group = m.group(1);
+            		int index = group.indexOf(':');
+            		if (index >= 0) {
+            			group = group.substring(0, index);
+            		}
+            		return group;
+            	}
+            	return errorMsg;
+            }
             return null;
         } catch (Exception e) {
             e.printStackTrace();
