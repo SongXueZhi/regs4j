cdc071dfec652e1612a5f568397f632a8bc82328
DavidSun <20212010146@fudan.edu.cn>
2022-04-18 20:25:46 +0800

    update configs && Example

--- src/main/java/core/Configs.java
+++ src/main/java/core/Configs.java
@@ -2,7 +2,7 @@ package core;
 
 public class Configs {
     public   final  static  String workSpace="/home/sxz/Documents/test-transfer-space";
-    public final static String URL="jdbc:mysql://10.177.21.179:3306/regression?useSSL=false&allowPublicKeyRetrieval=true&characterEncoding=UTF8";
+    public final static String URL="jdbc:mysql://10.177.21.179:3306/regminer_regression?useSSL=false&allowPublicKeyRetrieval=true&characterEncoding=UTF8";
     public final static String NAME="root";
     public final  static String PWD="123456";
 }
