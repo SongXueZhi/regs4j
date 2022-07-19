47f4556ba2699c347a8faf8325d479446a576fb4
SongXueZhi <sxzdhu@gmail.com>
2022-07-19 09:08:55 +0800

    add hunk entity

---
+++ src/main/java/model/HunkEntity.java
@@ -0,0 +1,22 @@
+package model;
+
+/**
+ * @Author: sxz
+ * @Date: 2022/07/19/09:05
+ * @Description:
+ */
+public class HunkEntity {
+    String newPath;
+    String oldPath;
+    String beginA;
+    String beginB;
+    String EndA;
+    String EndB;
+    String type;
+
+//    enum Type{
+//        DELETE,
+//        REPLACE,
+//        ADD
+//    }
+}
