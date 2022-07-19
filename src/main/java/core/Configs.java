477714cfd96592ac469dee652ac409edf114f6fa
shuning640 <21210240023@m.fudan.edu.cn>
2022-07-19 10:04:04 +0800

    add uuid_select

--- src/main/java/core/Configs.java
+++ src/main/java/core/Configs.java
@@ -4,8 +4,8 @@ import java.io.File;
 
 public class Configs {
     //    public   final  static  String workSpace= System.getProperty("user.home") + "/Documents/miner-space";
-    public final static String workSpace = System.getProperty("user.home") + File.separator + "reg_space";;
-    public final static String URL = "jdbc:mysql://10.177.21.179:3306/code_annotation?useSSL=false&allowPublicKeyRetrieval=true&characterEncoding=UTF8";
+    public final static String workSpace = System.getProperty("user.home") + File.separator + "reg_space";
+    public final static String URL = "jdbc:mysql://10.177.21.179:3306/code_annotation2?useSSL=false&allowPublicKeyRetrieval=true&characterEncoding=UTF8";
     public final static String NAME = "root";
     public final static String PWD = "123456";
 }
